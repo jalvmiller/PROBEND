33c701e27eae430ee5d1e2475e2931c3c6b9f964
Joao <muller.rok16@outlook.com>
2026-02-01 18:55:37 -0300

    inicio com springboot, uso do initializer; criação de arquivos, pastas

---
+++ sistemasquestoes/src/main/java/com/joaomu/sistemasquestoes/SistemasquestoesApplication.java
@@ -0,0 +1,13 @@
+package com.joaomu.sistemasquestoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasquestoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasquestoesApplication.class, args);
+	}
+
+}
