7150ca8a8e3175741ed149f36a16f398209666cc
Joao <muller.rok16@outlook.com>
2026-02-14 18:26:08 -0300

    Typo no pom.xml, linha 35; criação dos DTOs criação de métodos para o user e o UserRepository

--- sistemasquestoes/src/main/java/br/com/joaomu/model/User.java
+++ sistemasquestoes/src/main/java/br/com/joaomu/model/User.java
@@ -37,4 +37,14 @@ public class User implements UserDetails {
         return List.of();
     }
     
+
+    // Deixar tudo como true, mudar caso implemente algum tipo de bloqueio eventualmente
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+    @Override
+    public boolean isEnabled() { return true; }
 }
---
+++ sistemasquestoes/src/main/java/br/com/joaomu/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.joaomu.repo;
+
+import br.com.joaomu.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
