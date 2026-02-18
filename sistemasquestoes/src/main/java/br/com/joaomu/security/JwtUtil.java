972d38fafc21f73f4afa67c988fd71c2df34ded2
Joao <muller.rok16@outlook.com>
2026-02-18 20:04:37 -0300

    segurança; uso do spring security: classes e dependências para que funcione com o projeto

--- sistemasquestoes/src/main/java/br/com/joaomu/security/JwtUtil.java
+++ sistemasquestoes/src/main/java/br/com/joaomu/security/JwtUtil.java
@@ -16,7 +16,7 @@ public class JwtUtil {
     private final long EXPIRATION_TIME = 86400000; // 24 horas em ms
 
     private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes()); // CONVERSÃO HMAC-SHA, padrão do JWT.. geração de chave segura
     }
 
     public String generateToken(String username) {
@@ -24,9 +24,10 @@ public class JwtUtil {
             .subject(username)
             .issuedAt(new Date())
             .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-            .signWith( getSigningKey() )
-            .compact();
+            .signWith( getSigningKey() ) // assinar o token com a chave
+            .compact(); // token é gerado como string compacta, padrão da doc do JWT
     }
+
 
     public String extractUsername(String token) {
         return Jwts.parser()
@@ -37,6 +38,7 @@ public class JwtUtil {
             .getSubject();
     }
 
+    
     public boolean validateToken(String token) {
         try {
             Jwts.parser()
