561bb12bfa3226a2e9a5fb3d793d6dadebd82e19
Joao <muller.rok16@outlook.com>
2026-03-01 14:15:21 -0300

    problemas com o package.json; uso de CRA ao inves do vite; correção: nova pasta para o frontend

---
+++ frontend-novo/src/App.jsx
@@ -0,0 +1,20 @@
+// import
+import './App.css'
+
+// Componente
+function App() {
+
+  return (
+    <div className="container-teste">
+      <h1>PROBENDO - Sistema de Questões</h1>
+      <p>Teste de componente do React</p>
+
+      <button onClick={() => alert('React funcionando')}>
+        Teste
+      </button>
+    </div>
+  )
+}
+
+// export
+export default App
