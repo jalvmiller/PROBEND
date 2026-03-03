288c10c80f2ae836a14a310d652a701cabea3a47
Joao <muller.rok16@outlook.com>
2026-03-03 14:15:12 -0300

    aplicação do tailwind; index.css só contém o ponto de entrada do tailwind agora

--- frontend-novo/src/App.jsx
+++ frontend-novo/src/App.jsx
@@ -1,34 +1,32 @@
 // import
-import './App.css'
+// não mais -> import './App.css'
+import QuestaoCard from "./components/QuestaoCard";
 
 // Componente
 function App() {
 
 
   return (
-    <div className="bg-slate-900 min-h-screen flex items-center justify-center p-6">
+    <div className="min-h-screen bg-slate-100 p-8 flex flex-col items-center">
+      <h1 className="text-4xl font-black text-slate-900 mb-8 tracking-tighter">PROBEND</h1>
 
-      <div className="bg-white rounded-2xl shadow-2xl p-8 max-w-md w-full border-l-8 border-blue-500">
-        <span className="text-blue-600 font-bold text-sm uppercase tracking-widest">
-          Java+Spring
-        </span>
+      <div className="max-w-2xl w-full">
+        {/* como se fosse uma tag HTML:*/}
+        <QuestaoCard
+          enunciado="O que é o Polimorfismo no Java?"
+          materia="Programação Orientada a Objetos"
+          dificuldade="Fácil"
+        />
 
-        <h1 className="text-2xl font-black text-gray-800 mt-2 mb-4">
-          Confg.
-        </h1>
-
-        <p className="text-gray-600 leading-relaxed mb-6">
-          CARD
-        </p>
-
-        ,<button className="w-full bg-blue-600 hover:bg-blue-700 text-white
-                           font-bold py-3 rounded-xl transition-all active:scale-95 shadow-lg">
-                            Muita coisa inline
-        </button>
+        <QuestaoCard
+          enunciado="Como funciona o Garbage Collector do Java 25?"
+          materia="Arquitetura de Sistemas"
+          dificuldade="Difícil"
+        />
       </div>
     </div>
   )
 }
 
 // export
-export default App
+export default App;
