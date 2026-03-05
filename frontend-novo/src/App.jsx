0aafb49f7389dc9a3779609a24686516819f026d
Joao <muller.rok16@outlook.com>
2026-03-05 13:48:10 -0300

    useState; processo de diffing do React, primeiro passo para usar o BD real

--- frontend-novo/src/App.jsx
+++ frontend-novo/src/App.jsx
@@ -1,50 +1,57 @@
 // import
 // não mais -> import './App.css'
+import { useState } from "react";
 import QuestaoCard from "./components/QuestaoCard";
 
 // Componente
 function App() {
 
-  const listaQuestoes = [
-    { /* OBJ 1 */
-      id: 101,
-      enunciado: "O que acontece se uma execção não for capturada?",
-      materia: "Java Exception",
-      dificuldade: "Fácil"
-    },
-    { /* OBJ 2 */
-      id: 102,
-      enunciado: "Conceito de imutabilidade de Strings",
-      materia: "Java",
-      dificuldade: "Médio"
-    },
-    { /* OBJ 3 */
-      id: 103,
-      enunciado: "Explique o que é o ciclo de vida de um singleton bean",
-      materia: "Spring",
-      dificuldade: "Difícil"
-    }
-  ];
+  // useState, primeira iteração com simulação de busca no backend via java/spring
+  //       getter             setter          lista vazia
+  const [listaQuestoes, setListaQuestoes] = useState([]);
+
+  // simulação
+  const buscaQuestoes = () => {
+    const resultQuestoes = [
+      { id: 1, enunciado: "oq e um bean", materia: "spring", dificuldade: "Fácil"},
+      { id: 2, enunciado: "2+2", materia: "matematica", dificuldade: "Fácil"}
+    ];
+
+    // atualização de estado. O react percebe a mudança e plota na hora
+    setListaQuestoes(resultQuestoes);
+  };
 
 
   return (
     <div className="min-h-screen bg-slate-100 p-8 flex flex-col items-center">
       <h1 className="text-4xl font-black text-slate-900 mb-8 tracking-tighter">PROBEND</h1>
 
+
+      <button
+        onClick={buscaQuestoes}
+        className="mb-8 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full shadow-lg transition-all"
+      >
+        Carregar informações!
+      </button>
+
+
+
       <div className="max-w-2xl w-full">
         {/* como se fosse uma tag HTML:*/}
         {/* array de objetos em uma lista de componentes*/}
         {/* listaQuestoes.map((questao)), mapeamento de cada item do array para um componente QuestaoCard*/}
         
-        
-        {listaQuestoes.map((questao) => (
+        {listaQuestoes === 0 ? (
+          <p className="text-gray-500 text-center bold">O banco de questões está vazio.</p>
+        ) : (
+        listaQuestoes.map((questao) => (
         <QuestaoCard
           key={questao.id} // OBRIGATÓRIO USAR NO REACT, igual ao @Id do JPA
           enunciado={questao.enunciado}
           materia={questao.materia}
           dificuldade={questao.dificuldade}
         />
-        ))}
+        )))}
 
 
       </div>
