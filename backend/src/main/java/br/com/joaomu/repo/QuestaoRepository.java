1e12bdb7ae1c83c7dedbbd14d5524ec4dc806668
Joao <muller.rok16@outlook.com>
2026-06-24 17:47:04 -0300

    docs: comentários nas classes de repositório

--- backend/src/main/java/br/com/joaomu/repo/QuestaoRepository.java
+++ backend/src/main/java/br/com/joaomu/repo/QuestaoRepository.java
@@ -5,12 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-// Classe Repository foi convertida para uma interface devido a aplicação do Jpa.
+/*
+    JpaRepository é
+    uma interface mãe que engloba os métodos do CrudRepository
+    e PagingAndSortingRepository.
+    Métodos como .save(), .findById(), .findAll() e .delete()
+    são gerados dinamicamente em tempo de execução pelo
+    Spring Data JPA através de proxies do Hibernate
+*/
 
 @Repository
 public interface QuestaoRepository extends JpaRepository<Questao, Long> {
 
-
+    // O Spring Data analisa a assinatura desses métodos 
+    // e gera a consulta SQL automaticamente baseado no nome do método.
     List<Questao> findByMateriaIgnoreCase(String materia);
 
     List<Questao> findByDificuldade(Integer dificuldade);
