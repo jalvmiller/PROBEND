a76583ccc55564fc83180d1c4a1bd011bfc4c633
Joao <muller.rok16@outlook.com>
2026-06-25 14:53:16 -0300

    fix: erro de CORS, path errado em SecurityConfig

--- backend/src/main/java/br/com/joaomu/security/SecurityConfig.java
+++ backend/src/main/java/br/com/joaomu/security/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173") // URL padrão do Vite, mudar quando for dar deploy já
                                                                  // que
                                                                  // o Vercel vai usar outro.
