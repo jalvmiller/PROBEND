dbd7f50033f0f92436dd02ebd7abcaf4819ede94
Joao <muller.rok16@outlook.com>
2026-06-28 11:01:45 -0300

    feat: opção de logout no frontend, feedback user
    - nome do usuário logado na Navbar
    - opção de logout integrada a um botão da navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,21 @@
 // import do ícone
 import { Menu } from 'lucide-react';
+import { LogOut } from 'lucide-react';
 
 // Navbar fica no topo da página
 // Função que recebe prop onMenuClick, quando alguém clica no botão do menu
 // chama essa função
 function Navbar({ onMenuClick }) {
-    
+
+  const { user, logout } = useAuth();
+
   return (
     // bg blue 600 -> fundo azul
     // text white  -> texto branco
     // shadow lg   -> sombra embaixo da barra
     <nav className="bg-blue-600 text-white shadow-lg">
 
-        {/*
+      {/*
             max-w-7xl       -> largura máxima
             mx-auto         -> centralizar horizontalmente
             px-4            -> espaçamento horizontal interno 1 rem no eixo x
@@ -42,9 +45,23 @@ function Navbar({ onMenuClick }) {
 
         {/* Logo/Titulo */}
         <h1 className="text-2xl font-bold">PROBEND</h1>
-
+        {user ? (
+          <div className="flex items-center gap-4">
+            <span className="text-sm font-medium hidden sm:inline">Olá, {user.nome}</span>
+            <button
+              onClick={logout}
+              className="flex items-center gap-1 
+                text-sm bg-blue-700 hover:bg-blue-800
+                px-3 py-2 rounded 
+                transition font-semibold">
+              <LogOut size={16} />
+              <span>Sair</span>
+            </button>
+          </div>
+        ) : (
+          <div className='w-10' />
+        )}
         {/* Espaço vazio colocado na direita da navbar para balancear */}
-        <div className="w-10" />
       </div>
     </nav>
   );
