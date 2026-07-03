6f4b35f25034c76b1c5c743c5d2dd32766dd9ad7
Joao <muller.rok16@outlook.com>
2026-07-03 00:57:38 -0300

    refact: listarTodas em questaoService usa termo
    - a chamada listarTodas em questaoService usa
    de termo e, caso não haja termo, usa a busca
    geral e retorna todas as questões

--- frontend/src/services/questaoService.js
+++ frontend/src/services/questaoService.js
@@ -5,18 +5,29 @@ import api from "./api";
 // Todas as chamadas HTTP
 
 export const questaoService = {
-  listarTodas: async () => {
-    const response = await api.get("/questoes");
-    return response.data;
-  },
-  
   salvar: async (questao) => {
     const response = await api.post("/questoes", questao);
     return response.data;
   },
-  
+
   excluir: async (id) => {
     const response = await api.delete(`/questoes/${id}`);
+    return response.data;
+  },
+
+  // Busca condicionada pelo ID, usada pela Dashboard
+  busca: async (busca) => {
+    // busca fica entre chaves {}, caso contrário 
+    // (busca) não é reconhecido como objeto, e 
+    // sim como uma String pura
+
+    // Se a variável 'busca' existir, adiciona 
+    // ela como parâmetro na requisição. Caso contrário, 
+    // envia {} e liste todas as questões
+    const response = await api.get("/questoes", {
+      params: busca ? { busca } : {}
+    });
+
     return response.data;
   }
 };
