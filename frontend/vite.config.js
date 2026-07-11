5f4c792d365e8b940f4f1147575acc5248906b15
Joao <muller.rok16@outlook.com>
2026-07-11 13:21:58 -0300

    feat: Path alias, com src sendo a pasta "@"
    - agora, basta usar import from "@[...]" que
    VSCode e Vite vão entender que o alias usa
    a pasta raiz do projeto
    - é bem mais conveniente usar o sistema de módulos
    do node dessa maneira

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url' // utilitário para path do node
 
 import tailwindcss from '@tailwindcss/vite' // importar o plugin do tailwind
 
@@ -9,4 +10,15 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      // fileURLToPath para que o vite saiba onde está a pasta src
+      // import.meta.url é uma variável global que contém o path absoluto
+      // do módulo atual. O "./src" é o caminho relativo da raiz do projeto
+      // e o "./src" indica a pasta que será aliasada. Com isso, dá pra
+      // importar arquivos usando @/ nome_do_arquivo em vez de ../nome_do_arquivo
+      // O Vite entende isso automaticamente
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
