c6d144c0ac0321c4d79e02dc248e72b53f9b8c2c
Joao <muller.rok16@outlook.com>
2026-07-26 20:29:56 -0300

    fix: reordenação dos imports do CodeBlock
    - o prismjs usa de herança dentro dos imports,
    c -> cpp -> csharp
    - a ordenação errada causava erro

--- frontend/src/utils/CodeBlock.jsx
+++ frontend/src/utils/CodeBlock.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useRef } from "react";
 import Prism from "prismjs";
 
 import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-csharp";
 import "prismjs/components/prism-java";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-python";
-import "prismjs/components/prism-csharp";
-import "prismjs/components/prism-cpp";
 import "prismjs/components/prism-css";
-import "prismjs/components/prism-c";
 import "prismjs/components/prism-sql";
 
 // Mapeamento para padronizar as Strings
